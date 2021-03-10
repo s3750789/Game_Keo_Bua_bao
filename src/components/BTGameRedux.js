40f7af8fab455aaaa342f41a35186c4a60649bec
Han <s3750789@rmit.edu.vn>
2021-03-10 23:01:35 +0700

    first commit

---
+++ src/components/BTGameRedux.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ThongTin from './ThongTin'
+
+export default class BTGameRedux extends Component {
+    render() {
+        return (
+            <div>
+                Bài tập game oẳn tù xì
+                {ThongTin}
+            </div>
+        )
+    }
+}
---
+++ src/components/ThongTin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class ThongTin extends Component {
+    render() {
+        return (
+            <div className = "col-4">
+                <h3>I'm iron man, I love you 3000</h3>
+                <h3>So ban thang : 0</h3>;
+                <h3>So ban choi: 0</h3>
+                <button style = {{position : center}} className = "btn btn-info">Play game</button>
+            </div>
+        )
+    }
+}
